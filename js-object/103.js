57bb3edd1ad8860417eaa7e01732d78237ac1d70
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-03-26 11:36:41 +0600

    Unary operation is allowed inside a string expression & that converts the string representation of the number to numeric value - null IS EQUIVALENT to false but NOT EQUAL to false

--- js-object/103.js
+++ js-object/103.js
@@ -26,3 +26,13 @@ objTwo.name = "object two name";
 
 print(objOne.name);
 print(objTwo.name);
+
+print('name' in objOne);//true
+print(objOne.hasOwnProperty('name'));//false
+
+print('name' in objTwo);//true
+
+//Poi: Because objTwo OVERRIDES 'name' property by setting 'name' to "object two name" objTwo NOW has 'name'
+//as OWN PROPERY. In other words, 'objTwo' doesn't need to FALL BACK to the 'name' property that is defined
+//at 'objBluePrint''s prototype
+print(objTwo.hasOwnProperty('name'));//true
