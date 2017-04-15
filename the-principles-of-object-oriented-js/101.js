4614d5fc204fe6dc24920be284e072a3a42f82be
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-04-16 00:11:13 +0600

    Equality of object is done on reference not on value (key - value tuple) - typeof null will return object and that is a bug in the programming language itself

--- the-principles-of-object-oriented-js/101.js
+++ the-principles-of-object-oriented-js/101.js
@@ -71,3 +71,34 @@ print(typeof {});//object
 //POI: In this case there no impact of using double equal
 print(obj == null);//true
 print(obj === null);//true
+
+var oneObj = { keyOne: 2 };
+
+print(oneObj);
+
+//POI: This is a reference copy. Not value copy
+var twoObj = oneObj;
+
+print(twoObj);
+
+//POI: This will have impact on 'oneObj'
+twoObj.keyOne = 3;
+
+print(oneObj);
+print(twoObj);
+
+//POI: Both will return true because comparison will be on reference
+print(oneObj == twoObj);
+print(oneObj === twoObj);
+
+var threeObj = { keyOne: 2 };
+
+//POI: Both of the comparison will return 'false' because comparison will be done on reference not on value
+print(oneObj == threeObj);
+print(oneObj === threeObj);
+
+var fourObj = Object.create(oneObj);
+
+//POI: In this case too, both comparison will return 'false' because 'fourObj' is a new object & the reference in heap is different from 'oneObj'
+print(oneObj == fourObj);
+print(oneObj === fourObj);
