bf661d167793598733b49d5c07e33f6e9582f190
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-04-15 23:59:50 +0600

    More of JS OOP

--- the-principles-of-object-oriented-js/101.js
+++ the-principles-of-object-oriented-js/101.js
@@ -38,3 +38,36 @@ var ref;
 print(obj);
 print(flag);
 print(ref);
+
+var color = "red";
+var anotherColor = color;
+
+print(color);
+print(anotherColor);
+
+//POI: This assignment will not have any impact on 'color' because color is a primitive type. So only the value is copied into 'anotherColor'
+//POI: This is similar to C# Strings where also the value will be copied into another string not the reference
+anotherColor = "blue";
+
+print(color);
+print(anotherColor);
+
+//POI: To identify PRIMITIVE types 'typeof' should be used. It will provide proper type except for 'null'
+print(typeof name);
+print(typeof selection);
+
+print(typeof number);
+print(typeof cost);
+
+print(typeof isFalse);
+print(typeof isTrue);
+
+print(typeof flag);
+
+//POI: Value of 'obj' is 'null' but it's a JS bug to consider 'null' as object. To compare null use '==='
+print(typeof obj);//object
+print(typeof {});//object
+
+//POI: In this case there no impact of using double equal
+print(obj == null);//true
+print(obj === null);//true
