67f990cbef1046b61026fb17e60b1b91c054697e
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-04-15 22:50:30 +0600

    Object Oriented JS - 101

---
+++ the-principles-of-object-oriented-js/101.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+function print(msg) {
+    console.log(msg);
+}
+
+//String
+var name = "Nicholas";
+var selection = "a";
+
+print(name);
+print(selection);
+
+//Number
+var number = -56;
+var cost = 0.23;
+
+print(number);
+print(cost);
+
+//Boolean
+var isTrue = true;
+var isFalse = false;
+
+print(isTrue);
+print(isFalse);
+
+//null
+var obj = null;
+
+//undefined
+var flag = undefined;
+
+//POI: This will assign 'undefined' to 'ref' automatically
+var ref;
+
+print(obj);
+print(flag);
+print(ref);
