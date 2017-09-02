aac6679739d1c227639dd54a72c83500aa2c7d75
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-09-02 21:06:23 +0600

    Multiple component

--- angular-js/tour-of-heroes/src/app/app.component.ts
+++ angular-js/tour-of-heroes/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-// templateUrl: './app.component.html',
+import { Hero } from './hero'
+import { HeroDetailComponent } from './hero-detail.component'
 
 @Component({
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
+
+  <hero-detail [hero]='selectedHero'></hero-detail>
+
   <div class="hero">
-    <input [(ngModel)]="hero.name" placeholder="name">
+    <ul class="heroes">
+      <li *ngFor="let hero of heros" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
   </div>
   `,
   styleUrls: ['./app.component.css']
@@ -20,8 +22,19 @@ export class Hero {
 
 export class AppComponent {
   title = 'My First Angular App';
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  selectedHero: Hero;
+  heros: Hero[] = HEROS;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROS: Hero[] = [
+  { id: 10, name: "Mr. Nice" },
+  { id: 20, name: "Narco" },
+  { id: 30, name: "Bombasto" },
+  { id: 40, name: "Celeritas" },
+  { id: 50, name: "Magneta" },
+  { id: 60, name: "RubberMan" },
+];
