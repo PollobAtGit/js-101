11e64637b79a9d8974b6f5dcace1b774ba78412c
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-09-04 01:19:28 +0600

    ngIf ngFor trackBy & many more

--- angular-js/tour-of-heroes/src/app/app.module.ts
+++ angular-js/tour-of-heroes/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component'
 import { EmployeeComponent } from './employee/employee.component';
+import { KudComponent } from "./kud/kud.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    KudComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
 
   // POI: Change bootstrapped class name toggle start up component
-  bootstrap: [AppComponent]
+  bootstrap: [KudComponent]
 })
 export class AppModule { }
--- angular-js/tour-of-heroes/src/app/employee/employee.component.ts
+++ angular-js/tour-of-heroes/src/app/employee/employee.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core'
 @Component({
     // POI: 'app-root' because Employee is used as the start up element
     selector: "app-root",
-    templateUrl: "employee.component.html"
+    templateUrl: "employee.component.html",
+    styleUrls: ["./employee.component.css"]
 })
 
 export class EmployeeComponent {
@@ -13,8 +14,19 @@ export class EmployeeComponent {
     age: number = 10;
 
     webPage: string = "https://www.w3schools.com/css/img_fjords.jpg";
+    isButtonDisabled: boolean = false;
+    tableColSpan: number = 10;
+    isBold: boolean = true;
+    aquaClass: string = 'aqua-color';
 
     getFullName(): string {
         return this.firstName + " " + this.lastName;
+    }
+
+    applyMultipleCls() {
+        return {
+            "red-color": true,
+            "text-align": false
+        };
     }
 }
