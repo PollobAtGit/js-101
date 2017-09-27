15fb6cb0bccdc55eb23bdab75aa9e5f09474e06a
Sheikh Ashiqul islam <pollob60@live.com>
2017-09-27 17:54:48 +0600

    File up-loader & HTML file server

--- node-js/w3-school/node-server.js
+++ node-js/w3-school/node-server.js
@@ -1,31 +1,76 @@
 
-var http = require('http');
-var url = require('url');
-var fileServer = require('fs');
-var dateModule = require('./current-date-module.js');
+var httpMod = require('http');
+var urlMod = require('url');
+var fileServerMod = require('fs');
+var dateMod = require('./current-date-module.js');
+var upperCaseMod = require('upper-case');
+var eventsMod = require('events');
+var formidableMod = require('formidable');
 
-http.createServer(function(req, res) {
-    
-    fileServer.readFile('s.html', function(err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-    
-        var queryString = url.parse(req.url, true);
-        console.log(queryString.query);
-        console.log(queryString.query.id);
-    
-        res.write(data);
-    
-        fileServer.appendFile('s-clone.html', data, function(err) {
-            if(err) {
-                throw err;
-            }
-            console.log('SAVED!');
+var eventEmitter = new eventsMod.EventEmitter();
+
+var prnt = function (data) {
+    console.log(data);
+}
+
+// POI: Registering a event & it's event handler
+// POI: Custom event. It's not defined from earlier
+eventEmitter.on('invoke', () => console.log("Invoking Print"));
+
+httpMod.createServer(function (req, res) {
+
+    if (req.url === '/upload-file') {
+
+        prnt('Attempt to upload file');
+
+        var form = formidableMod.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            fileServerMod.rename(files.fileToUpload.path, files.fileToUpload.name, (err) => {
+
+                var message = "";
+
+                if (err) {
+                    message = "SOME WENT WRONG WITH FILE UPLOAD";
+                } else {
+                    message = "file upload successful";
+                }
+
+                res.write(upperCaseMod(message), (err) => res.end());
+            });
         });
+    } else {
 
-        res.end();
-    });
+        var pageToServe = urlMod.parse(req.url, true).query.page.concat(".html");
+
+        console.log("Attempting to serve: " + pageToServe);
+
+        if (pageToServe) {
+            fileServerMod.readFile(pageToServe, function (err, data) {
+
+                var errorCode = 0;
+                var contentType = 'text/html';
+                var fileContent = '';
+
+                if (!err) {
+                    errorCode = 200;
+                    fileContent = data;
+                } else {
+                    errorCode = 200;
+                    fileContent = upperCaseMod("File Doesn't Exists");
+                }
+
+                res.writeHead(errorCode, { 'Content-Type': contentType });
+                res.write(fileContent, (err) => res.end());
+            });
+        } else {
+            // TODO: Code will never reach here
+            res.write(upperCaseMod('Query string is empty'), (err) => res.end());
+        }
+    }
+
+    // POI: Emitting a event
+    eventEmitter.emit('invoke');
 
 }).listen(8080);
 
 console.log('Server Is Running');
-    //res.write('\r\nHello World! Node Server Is Running & Current Time Is: ' + dateModule.myDate());
