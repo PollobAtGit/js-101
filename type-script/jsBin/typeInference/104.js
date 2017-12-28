743223c485722b552ffd486ef1b4a7212f32295f
Sheikh Ashiqul Islam Pollob <pollob60@live.com>
2017-12-29 01:17:24 +0600

    Function overloading, rest argument & optional argument for callback

--- type-script/jsBin/typeInference/104.js
+++ type-script/jsBin/typeInference/104.js
@@ -10,6 +10,7 @@ var __extends = (this && this.__extends) || (function () {
 })();
 var OneZeroFour;
 (function (OneZeroFour) {
+    var cl = function (v) { return console.log(v); };
     var Event = (function () {
         function Event() {
         }
@@ -22,14 +23,62 @@ var OneZeroFour;
         }
         return MouseEvent;
     }(Event));
+    var KeyEvent = (function () {
+        function KeyEvent() {
+        }
+        return KeyEvent;
+    }());
     var listenEvent = function (h) { };
     var anotherListenEvent = function (h) { };
     listenEvent(function (e) { });
     anotherListenEvent(function (e) { });
+    // KFU! Doesn't make sense because KeyEvent is structurally different from Event
+    listenEvent(function (e) { });
+    // KFU! Doesn't make sense number/string/boolean has nothing to do with Event
+    // KFU! No type checking is being done
+    // Explanation! Type checking fails when class & interfaces have no member because in this case eveything
+    // is assignable to them
+    listenEvent(function (e) { });
+    listenEvent(function (e) { });
+    listenEvent(function (e) { });
     // POI: From JS's respect it doesn't matter because it doesn't care about the type.
     // Interpreter will simply invoke the object property
     // POI: Passing more specific type as function argument (polymorphic behavior)
     listenEvent(function (e) { });
     // POI: Passing less specific type as function argument (contravariance?)
     anotherListenEvent(function (e) { });
+    var invokeLater = function (cb) { };
+    // POI: Because of rest argument we can pass any number of parameters
+    invokeLater(function (x) { });
+    invokeLater(function (x, y) { });
+    invokeLater(function (x, y, z) { });
+    // POI: Because of rest argument we can pass function that has any number of optional parameters
+    invokeLater(function () { });
+    // POI: This is semantically same as providing any of
+    // () : void = { }
+    // (x) : void = { }
+    // (x, y) : void = { }
+    // Note that it's not possible to ommit x but provide y
+    invokeLater(function (x, y) { });
+    // POI: Overloaded function definition must follow after the overloaded function list. Only
+    // comments are allowed in between
+    function xListenMethod(e) {
+        if (typeof e === "string") {
+            cl("STRING");
+        }
+        if (typeof e === "object") {
+            cl("OBJECT");
+        }
+    }
+    xListenMethod("WITH STRING");
+    xListenMethod(new Event());
+    var nonVoid = function (cb) { return cb(); };
+    // KFU! Callback returns void but passed callback returns non-void
+    // Apparently a better explanation is void indicates compiler won't care about what callback function returns
+    nonVoid(function () { return 10; });
 })(OneZeroFour || (OneZeroFour = {}));
+// IMPORTANT: Type checking is sloopy in case of function argument
+// IMPORTANT: For callback less specific type is assignable to more specific type & vice versa
+// IMPORTANT: For callback optional arguments & rest argument is interchangeable
+// IMPORTANT: Type checking fails for class & interfaces when they have no member
+// TODO: Prove above statement
--- type-script/typeInference/104.ts
+++ type-script/typeInference/104.ts
@@ -1,14 +1,29 @@
 
 namespace OneZeroFour {
 
+    const cl = (v: any) => console.log(v);
+
     class Event { }
     class MouseEvent extends Event { }
+    class KeyEvent { keyCode: number; }
 
     const listenEvent = (h: (e: Event) => void) => { };
     const anotherListenEvent = (h: (e: MouseEvent) => void) => { };
 
     listenEvent((e: Event): void => { });
     anotherListenEvent((e: MouseEvent): void => { });
+
+    // KFU! Doesn't make sense because KeyEvent is structurally different from Event
+    listenEvent((e: KeyEvent): void => { });
+
+    // KFU! Doesn't make sense number/string/boolean has nothing to do with Event
+    // KFU! No type checking is being done
+    // Explanation! Type checking fails when class & interfaces have no member because in this case eveything
+    // is assignable to them
+
+    listenEvent((e: number): void => { });
+    listenEvent((e: string): void => { });
+    listenEvent((e: boolean): void => { });
 
     // POI: From JS's respect it doesn't matter because it doesn't care about the type.
     // Interpreter will simply invoke the object property
@@ -17,4 +32,52 @@ namespace OneZeroFour {
     listenEvent((e: MouseEvent): void => { });
     // POI: Passing less specific type as function argument (contravariance?)
     anotherListenEvent((e: Event): void => { });
+
+    const invokeLater = (cb: (...arg: number[]) => void) => { };
+
+    // POI: Because of rest argument we can pass any number of parameters
+    invokeLater((x): void => { });
+    invokeLater((x, y): void => { });
+    invokeLater((x, y, z): void => { });
+
+    // POI: Because of rest argument we can pass function that has any number of optional parameters
+    invokeLater((): void => { });
+
+    // POI: This is semantically same as providing any of
+    // () : void = { }
+    // (x) : void = { }
+    // (x, y) : void = { }
+    // Note that it's not possible to ommit x but provide y
+
+    invokeLater((x?, y?): void => { });
+
+    // POI: This is not allowed because it's not possible to distinguish values between x & y when x is
+    // optional & first argument
+    // invokeLater((x?, y): void => { });
+
+    function xListenMethod(e: Event): void;
+    function xListenMethod(e: string): void;
+
+    // POI: Overloaded function definition must follow after the overloaded function list. Only
+    // comments are allowed in between
+    function xListenMethod(e: any): void {
+        if (typeof e === "string") { cl("STRING"); }
+        if (typeof e === "object") { cl("OBJECT"); }
+    }
+
+    xListenMethod("WITH STRING");
+    xListenMethod(new Event());
+
+    const nonVoid = (cb: () => void): void => cb();
+
+    // KFU! Callback returns void but passed callback returns non-void
+    // Apparently a better explanation is void indicates compiler won't care about what callback function returns
+    nonVoid((): number => 10);
 }
+
+// IMPORTANT: Type checking is sloopy in case of function argument
+// IMPORTANT: For callback less specific type is assignable to more specific type & vice versa
+// IMPORTANT: For callback optional arguments & rest argument is interchangeable
+
+// IMPORTANT: Type checking fails for class & interfaces when they have no member
+// TODO: Prove above statement
