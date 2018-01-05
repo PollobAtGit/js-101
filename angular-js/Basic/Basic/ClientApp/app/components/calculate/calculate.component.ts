7d596678463df79bb70e234d865b6cacc961bc80
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-01-05 20:46:59 +0600

    Angular 201

---
+++ angular-js/Basic/Basic/ClientApp/app/components/calculate/calculate.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'calculate',
+    templateUrl: './calculate.component.html',
+    styleUrls: ['./calculate.component.css']
+})
+
+export class CalculateComponent { }
