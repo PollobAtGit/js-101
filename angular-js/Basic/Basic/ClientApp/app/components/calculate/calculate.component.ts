c75d237e411d9916ebfc4401921c3074439b040e
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-01-07 21:50:58 +0600

    ng - working calculator

--- angular-js/Basic/Basic/ClientApp/app/components/calculate/calculate.component.ts
+++ angular-js/Basic/Basic/ClientApp/app/components/calculate/calculate.component.ts
@@ -6,4 +6,39 @@
     styleUrls: ['./calculate.component.css']
 })
 
-export class CalculateComponent { }
+export class CalculateComponent {
+
+    firstNumber: number;
+    secondNumber: number;
+    result: number;
+
+    constructor() { }
+
+    add() {
+
+        if (this.skipCalculation()) { return; }
+
+        this.result = this.firstNumber + this.secondNumber;
+    }
+
+    sub() {
+
+        if (this.skipCalculation()) { return; }
+        this.result = Math.abs(this.firstNumber - this.secondNumber);
+    }
+
+    mul() {
+
+        if (this.skipCalculation()) { return; }
+        this.result = this.firstNumber * this.secondNumber;
+    }
+
+
+    private skipCalculation() {
+        return Number.isNaN(this.firstNumber)
+            || !Number.isFinite(this.firstNumber)
+            || Number.isNaN(this.secondNumber)
+            || !Number.isFinite(this.secondNumber);
+    }
+
+}
