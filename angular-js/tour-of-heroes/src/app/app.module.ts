fa5311bf81952c458fb273d0189b15f3eedf297f
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-01-22 22:30:44 +0600

    ng - heroes component

--- angular-js/tour-of-heroes/src/app/app.module.ts
+++ angular-js/tour-of-heroes/src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
+
+  // POI: Why do we need declarations?
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
+
+  // TODO: Why we need imports at all?
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
+
+  // TODO: Why providers are for?
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ angular-js/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "./../hero";
+
+@Component({
+  selector: 'heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = new Hero(1, "Windstorm");
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
