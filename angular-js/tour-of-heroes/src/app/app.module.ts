efb60e6eeb59ee1f4e8b2850b8c15faaaf12d34d
Sheikh Ashiqul Islam Pollob <meet.pollob@gmail.com>
2018-01-30 01:01:49 +0600

    ng routing

--- angular-js/tour-of-heroes/src/app/app.module.ts
+++ angular-js/tour-of-heroes/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessageComponent } from './message/message.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -17,13 +19,15 @@ import { MessageService } from './message.service';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessageComponent
+    MessageComponent,
+    DashboardComponent
   ],
 
   // TODO: Why we need imports at all?
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
 
   providers: [HeroService, MessageService],
